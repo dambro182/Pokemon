f2847230f01f5d9f647664e71dbb4118515fc3d0
tomas Dambrosio <dambro184@gmail.com>
2022-11-10 20:09:28 -0300

    ultima version para entregar

--- main.js
+++ main.js
@@ -16,10 +16,12 @@ const pokemon3 = {
   Tipo: "Agua",
   Daño: 28,
 };
+
 let opcion1 = pokemon1;
 let opcion2 = pokemon2;
 let opcion3 = pokemon3;
 
+const pokemonContainer = document.querySelector(".title1");
 //Almacenamiento De  Pokemones Seleccionados
 let Pokeselect = undefined;
 let enemySelect = undefined;
@@ -52,6 +54,64 @@ enemySelect3.addEventListener("click", () => {
   enemySelect = pokemon3;
 });
 
+if (Pokeselect != undefined) {
+  let Pokeselecthp = Pokeselect.Vida;
+}
+if (enemySelect != undefined) {
+  let enhp = enemySelect.Vida;
+}
+fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+  .then((response) => {
+    return response.json();
+  })
+  .then((user) => {
+    console.log(user);
+    document.getElementById("title1").innerHTML = user.name;
+  });
+fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+  .then((response) => {
+    return response.json();
+  })
+  .then((user) => {
+    console.log(user);
+    document.getElementById("title2").innerHTML = user.name;
+  });
+fetch("https://pokeapi.co/api/v2/pokemon/squirtle")
+  .then((response) => {
+    return response.json();
+  })
+  .then((user) => {
+    console.log(user);
+    document.getElementById("title3").innerHTML = user.name;
+  });
+fetch("https://pokeapi.co/api/v2/pokemon/squirtle").then((response) => {
+  return response.json();
+});
+fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+  .then((response) => {
+    return response.json();
+  })
+  .then((user) => {
+    console.log(user);
+    document.getElementById("title4").innerHTML = user.name;
+  });
+fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+  .then((response) => {
+    return response.json();
+  })
+  .then((user) => {
+    console.log(user);
+    document.getElementById("title5").innerHTML = user.name;
+  });
+fetch("https://pokeapi.co/api/v2/pokemon/squirtle")
+  .then((response) => {
+    return response.json();
+  })
+  .then((user) => {
+    console.log(user);
+    document.getElementById("title6").innerHTML = user.name;
+  });
+
 //boton Start Game
 const Boton = document.getElementById("start");
 
@@ -62,21 +122,42 @@ function combate() {
     let GolpePokemon1 = Math.ceil(Math.random() * Pokeselect.Daño);
     let GolpePokemon2 = Math.ceil(Math.random() * enemySelect.Daño);
     round = round + 1;
-    console.log(round);
+    let Pokeselecthp = Pokeselect.Vida;
+    let enemySelecthp = enemySelect.Vida;
+
     if (GolpePokemon1 > GolpePokemon2) {
       enemySelect.Vida -= GolpePokemon1;
-      document.write(
-        Pokeselect.Nombre + " " + "Hizo " + GolpePokemon1 + " Daño"
-      );
-      document.write(Pokeselect.Nombre + " Vida = " + Pokeselect.Vida);
-      document.write(enemySelect.Nombre + " Vida = " + enemySelect.Vida);
+      document.getElementById("miPagina").innerHTML +=
+        "<div id=final>" +
+        " este es el round " +
+        round +
+        "<br>" +
+        " tu vida restante es de " +
+        Pokeselecthp +
+        "<br>" +
+        " la vida restante del rival es " +
+        enemySelecthp +
+        "<br>";
     } else {
       Pokeselect.Vida -= enemySelect.Daño;
-      document.write(
-        enemySelect.Nombre + " " + "Hizo " + GolpePokemon2 + " Daño"
+      document.getElementById("miPagina").innerHTML +=
+        "<div id=final>" +
+        " este es el round " +
+        round +
+        "<br>" +
+        " tu vida restante es de " +
+        Pokeselecthp +
+        "<br>" +
+        " la vida restante del rival es " +
+        enemySelecthp +
+        "<br>";
+    }
+    if (Pokeselect.Vida <= -20) {
+      swal.fire(
+        "Tu Digimon se ha debilitado, la pelea duro " + round + " rounds"
       );
-      document.write(enemySelect.Nombre + " Vida = " + enemySelect.Vida);
-      document.write(Pokeselect.Nombre + " Vida = " + Pokeselect.Vida);
+    } else {
+      swal.fire("Tu digimon ha ganado, la pelea duro " + round + " rounds");
     }
   }
 }
